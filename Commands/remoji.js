4a5563652b455c262f0c69aa617ba58ebe5e90de
Tinelix Development <44694825+tinelix@users.noreply.github.com>
2020-08-16 08:47:10 +0700

    Update remoji.js

--- Commands/remoji.js
+++ Commands/remoji.js
@@ -1,7 +1,7 @@
 module.exports = {
 	name: 'remoji',
 	description: 'Случайно-генерируемые эмоции',
-	execute(message, client, args) {
+	execute(message, client, args, botconfig) {
 	  var t_log = {
       embed: {
         color: 0x558800,
@@ -32,7 +32,7 @@ module.exports = {
         ]
       }
     };
-      client.channels.cache.get("564022728143929370").send(t_log);
+      client.channels.cache.get(botconfig.log_channel).send(t_log);
       let emoji = [
         ":joy:",
         ":thinking:",
